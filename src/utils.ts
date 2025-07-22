a6075e9abdce11f3c167deb9bbc362df8b9baa60
Rahul Patel <whyorean@gmail.com>
2025-07-22 18:39:45 +0530

    use default user avatar from gooogle

--- src/utils.ts
+++ src/utils.ts
@@ -3,30 +3,9 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-function getRandomColor(): string {
-  const colors = [
-    "red",
-    "green",
-    "blue",
-    "yellow",
-    "purple",
-    "orange",
-    "pink",
-    "brown",
-    "gray",
-    "black",
-    "cyan",
-    "magenta",
-    "lime",
-    "teal"
-  ]
-
-  return colors[Math.floor(Math.random() * colors.length)]
-}
-
 function generateAvatar() {
   return {
-    url: `https://placehold.co/96x96/${getRandomColor()}/white?text=IMG`,
+    url: "https://lh3.googleusercontent.com/a/default-user",
     type: 4,
     width: 96,
     height: 96
